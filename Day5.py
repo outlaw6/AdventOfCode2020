b6d50f61782c08d2e2c30daf71091e63686374e6
outlaw6 <milan.damjanovic.mbs@gmail.com>
2021-02-06 15:09:29 +0100

    Final Day5

--- Day5.py
+++ Day5.py
@@ -10,7 +10,7 @@ for n in q:
 	print(n.strip('\n'))'''
 
 l1=[x for x in range(128)]
-l2=l1[:]
+
 print(l1)
 
 
@@ -21,5 +21,15 @@ for char in s1:
 		l1=l1[len(l1)//2:]
 	elif char == 'F':
 		l1=l1[:len(l1)//2]
-seats="RLR"
-print(l1)
+
+l2=[x for x in range(8)]
+s2="RLR"
+
+for char in s2:
+	if char == 'R':
+		l2=l2[len(l2)//2:]
+	elif char == 'L':
+		l2=l2[:len(l2)//2]
+print(l1,l2)
+
+print("Final seat is: " + str(int(l1[0]) * 8 + int(l2[0]))) # Sry for this mumbo jumbo 
