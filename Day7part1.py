fb33bade50c3262ed3c91bef5d7e8ec9402a511e
outlaw6 <milan.damjanovic.mbs@gmail.com>
2021-02-14 17:17:21 +0100

    Day7P2

--- Day7part1.py
+++ Day7part1.py
@@ -27,3 +27,11 @@ def can_hold(in_color, out_color):
 
 part1 = sum([can_hold('shiny gold', bag) for bag in bagsz])
 print(part1)
+
+def num_inside(color):
+    return sum([bagsz[color][b] * (1 + num_inside(b)) for b in bagsz[color]])
+
+
+part1 = sum([can_hold("shiny gold", bag) for bag in bags])
+print("Part 1: ",part1)
+print('Part 2: ',num_inside("shiny gold"))
